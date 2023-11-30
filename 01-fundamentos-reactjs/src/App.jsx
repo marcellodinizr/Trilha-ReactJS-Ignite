0e3d8d5869c56eba0e270eda33ba627d62bdd45f
marcellodinizr <marcellodinizrocha@gmail.com>
2023-11-30 12:18:25 -0300

    feat: css global and fonts

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,14 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import './global.css'
 
 export function App() {
 
   return (
 		<div>
+			<Header />
+
 			<Post 
 				author="Marcello Diniz"
 				content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus laoreet nunc erat, id pellentesque elit ultricies nec. Ut eget ipsum."
