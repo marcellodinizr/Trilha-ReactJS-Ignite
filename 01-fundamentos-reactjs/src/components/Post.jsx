c4801abc69a78edd3e5bda7af23816ab319cbfb5
marcellodinizr <marcellodinizrocha@gmail.com>
2024-03-12 11:02:45 -0300

    feat: delete comment

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -35,6 +35,14 @@ export function Post({ author, publishedAt, content }) {
 		setNewCommentText(event.target.value);
 	}
 
+	function deleteComment(commentToDelete) {
+		const commentsWithoutDeletedOne = comments.filter(comment => {
+			return comment !== commentToDelete;
+		})
+
+		setComments(commentsWithoutDeletedOne);
+	}
+
 	return (
 		<article className={styles.post}>
 			<header>
@@ -78,7 +86,11 @@ export function Post({ author, publishedAt, content }) {
 			<div className={styles.commentList}>
 				{comments.map(comment => {
 					return (
-						<Comment key={comment} content={comment} />
+						<Comment 
+							key={comment} 
+							content={comment} 
+							onDeleteComment={deleteComment}
+						/>
 					)
 				})}
 			</div>
