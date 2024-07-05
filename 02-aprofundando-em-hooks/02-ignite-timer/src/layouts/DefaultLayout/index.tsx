e9f3a99d614fea74ad335e3b7161694b329c9464
marcellodinizr <marcellodinizrocha@gmail.com>
2024-07-05 14:58:54 -0300

    feat: Header and new features

--- 02-aprofundando-em-hooks/02-ignite-timer/src/layouts/DefaultLayout/index.tsx
+++ 02-aprofundando-em-hooks/02-ignite-timer/src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/* This is where the child routes will be rendered */}
-    </div>
+    </LayoutContainer>
   )
 }
---
+++ 02-aprofundando-em-hooks/02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,42 @@
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput type="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
