41e55e23894ee7ca7b76d8d56c9dd1fb366e2d42
marcellodinizr <marcellodinizrocha@gmail.com>
2024-07-16 08:33:10 -0300

    feat: add TaskInput

--- 02-aprofundando-em-hooks/02-ignite-timer/src/pages/Home/index.tsx
+++ 02-aprofundando-em-hooks/02-ignite-timer/src/pages/Home/index.tsx
@@ -16,10 +16,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="task" />
+          <TaskInput
+            type="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
